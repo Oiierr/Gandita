778662e4db9ce3f00097c102b2b2085e647797a1
Oiierr <97726722+Oiierr@users.noreply.github.com>
2022-01-26 14:01:25 +0100

    1.0.2
    Nuf added cookies and dark mode

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,64 @@
-var tru = true
+var tru
+var element = document.body;
+var def = document.getElementById("style-default");
+var dark = document.getElementById("style-dark");
+
+
+window.onload = function() {
+    console.log(readDarkMode())
+    if(readDarkMode() == 'true') 
+        tru = true;
+    else 
+        tru = false
+    
+    blackWhite()
+};
+
 
 function blackWhite() {
-    console.log('Clicked: ' + tru)
-    if(tru) {
-        document.querySelector(".kulbtn").style.transform = "rotate(180deg)";
-        document.getElementById("bkground").src = '/assets/MIT_dark.jpg';
+    if (tru) {
+        try {
+            //document.querySelector(".kulbtn").style.transform = "rotate(180deg)";
+            document.getElementById("bkground").src = '/assets/MIT_dark.jpg';
+        } catch {}
+        def.disabled = "disabled";
+        dark.disabled = undefined;
+        createDarkMode(true)
         tru = false
     } else {
-        document.querySelector(".kulbtn").style.transform = "rotate(0)";
-        document.getElementById("bkground").src = '/assets/MIT_light.jpg';
+        try {
+            //document.querySelector(".kulbtn").style.transform = "rotate(0)";
+            document.getElementById("bkground").src = '/assets/MIT_light.jpg';
+        } catch {}
+        def.disabled = undefined;
+        dark.disabled = "disabled";
+        createDarkMode(false)
         tru = true
     }
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+}
+
+//Cookies 
+
+function createDarkMode(bool) {
+    console.log(`Created - darkMode Cookie: ${bool}`)
+    const d = new Date();
+    d.setTime(d.getTime() + (30*24*60*60*1000));//1month
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = 'darkMode' + "=" + bool + ";" + expires + ";path=/";
+}
+
+function readDarkMode() {
+    let name = 'darkMode' + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
